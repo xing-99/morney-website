0c622590cc9c46d331d6654739f08ac792c9bc14
xing <582158539@qq.com>
2021-02-23 00:46:58 +0800

    deploy

---
+++ precache-manifest.a7784ac0eb6979733d3e8bb8a0b67fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1180d88f82d308c92182",
+    "url": "/morney-website/css/app.b042cf54.css"
+  },
+  {
+    "revision": "7eb079c8d1eb46840e6ca4d5a5d992ac",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "1180d88f82d308c92182",
+    "url": "/morney-website/js/app.d70a78b7.js"
+  },
+  {
+    "revision": "8f5dde6ded4eba24c496",
+    "url": "/morney-website/js/chunk-vendors.e74e8675.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
